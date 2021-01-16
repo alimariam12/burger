80ca484c888af9a7db4ba52f73b6ee268b8de19d
Mariam Ali <alim12@csp.edu>
2021-01-15 22:46:43 -0600

    working on orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,48 @@
 let connection = require("../config/connection");
 
 let orm = {
-    selectALL: function(table, cb){
-        let queryString = 'SELECT * FROM' + tableName + ";";
-        connection.query(queryString, function(err, res){
-            if(err){
-                throw err;
-            }
-            cb(res);
-        });
-    }
-    insertOne: function(table, cols, vals, cb) {
-        let dbQuery = 
-        "INSERT INTO" + table + "(" + cols.toString() + ")" + "VALUES (" + createQmarks(vals.length) + ")"; 
-    }
-}
+  selectALL: function (tableName, cb) {
+    let queryString = `SELECT * FROM ${tableName};`;
+    connection.query(queryString, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
+  insertOne: function (tableName, cols, vals, cb) {
+    let queryString = `INSERT INTO ${tableName} (${cols}, devoured) VALUES ("${vals}")`;
+
+    console.log(queryString);
+
+    connection.query(queryString, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
+  updateOne: function (tableName, cols, booleans, condition, cb) {
+    let queryString = `UPDATE ${tableName} SET ${cols} = ${booleans} WHERE ${condition}`;
+
+    console.log(queryString);
+
+    connection.query(queryString, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
+  deleteOne: function (tableName, condition, cb) {
+    const queryString = `DELETE FROM ${tableName} WHERE ${condition}`;
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
+};
+
+module.exports = orm;
