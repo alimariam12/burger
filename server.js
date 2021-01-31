fbee02f05939a414d5bf4df15bbc9bb0815442ab
Mariam Ali <alim12@csp.edu>
2021-01-31 03:23:59 -0600

    changed PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParse = require("body-parser");
 const expressHbs = require("express-handlebars");
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
 
 app.use(express.static("public"));
 
@@ -16,6 +16,6 @@ app.set("view engine", "handlebars");
 let routes = require("../burger/controllers/burgers_controllers");
 app.use(routes);
 
-app.listen(PORT, function () {
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
