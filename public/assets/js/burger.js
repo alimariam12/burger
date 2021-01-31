e734b798dcf1eff48d57c85522c85eabf43b7712
Mariam Ali <alim12@csp.edu>
2021-01-31 01:36:28 -0600

    worked burger.js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -18,6 +18,18 @@ $(function () {
       location.reload();
     });
   });
+  $(".delete-burger").on("click", function (event) {
+    let id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted burger", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 
   $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
@@ -33,19 +45,6 @@ $(function () {
       data: newBurger,
     }).then(function () {
       console.log("created new burger");
-      // Reload the page to get the updated list
-      location.reload();
-    });
-  });
-
-  $(".delete-burger").on("click", function (event) {
-    let id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE",
-    }).then(function () {
-      console.log("deleted burger", id);
       // Reload the page to get the updated list
       location.reload();
     });
